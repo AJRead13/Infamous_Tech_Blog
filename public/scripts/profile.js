baa9089668ef45489f21a03cec520d5c1deb237c
Andrew Read <aj.read13@gmail.com>
2022-01-26 21:32:50 -0500

    updated views

--- public/scripts/profile.js
+++ public/scripts/profile.js
@@ -1,9 +1,9 @@
 const newFormHandler = async (event) => {
     event.preventDefault();
   
-    const name = document.querySelector('#project-name').value.trim();
-    const needed_funding = document.querySelector('#project-funding').value.trim();
-    const description = document.querySelector('#project-desc').value.trim();
+    const name = document.querySelector('#post-name').value.trim();
+    const needed_funding = document.querySelector('#post-info').value.trim();
+    const description = document.querySelector('#post-body').value.trim();
   
     if (name && needed_funding && description) {
       const response = await fetch(`/api/projects`, {
